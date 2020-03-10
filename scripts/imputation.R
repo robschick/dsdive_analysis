cb547a57fea41f87339e62036bd6cbf011422854
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-03-10 16:12:32 -0600

    refactoring imputation script for a) offsets, and b) config files

--- scripts/imputation.R
+++ scripts/imputation.R
@@ -1,12 +1,44 @@
 # parallel tools
-require(Rmpi, lib.loc = c('.', .libPaths()))
-require(snow, lib.loc = c('.', .libPaths()))
+require(Rmpi, lib.loc = c('singularity/libs', .libPaths()))
+require(snow, lib.loc = c('singularity/libs', .libPaths()))
 # modeling tools
-library(dsdive, lib.loc = c('.', .libPaths()))
+library(dsdive, lib.loc = c('singularity/libs', .libPaths()))
+# configuration tools
+library(composr, lib.loc = c('singularity/libs', .libPaths()))
+library(yaml, lib.loc = c('singularity/libs', .libPaths()))
 
 
 #
 # Set up environment
+#
+
+# read in configuration groups
+args = commandArgs(TRUE)
+if(length(args)>0) {
+  for(i in 1:length(args)){
+    eval(parse(text=args[[i]]))
+  }
+} else {
+  groups = NULL
+}
+rm(args,i)
+
+# build configuration
+cfg = compose_cfg(file = file.path('conf', 'config.yaml'), groups = groups)
+rm(groups)
+
+# output paths
+out.dir = file.path(cfg$base_paths$fit, cfg$data$name, cfg$subset$name, 
+                    cfg$validation$name, cfg$observation_model$name, 
+                    cfg$priors$name)
+dir.create(out.dir, recursive = TRUE)
+
+# load data and utility functions
+source(file.path('scripts', 'utils', 'datafns.R'))
+
+
+#
+# Set up parallel environment
 #
 
 # get or create cluster
@@ -19,44 +51,19 @@ if(is.null(cl)) {
 parallel::clusterSetRNGStream(cl = cl, NULL)
 
 # load modeling tools on nodes 
-clusterEvalQ(cl = cl, expr = library(dsdive, lib.loc = c(.libPaths(), '.')))
+clusterEvalQ(cl = cl, expr = library(dsdive, lib.loc = c('singularity/libs', 
+                                                         .libPaths())))
 
 
 #
-# load data and prior parameters for stage
+# load data
 #
 
-# identify dive files
-obs.path = file.path('data')
-dives = dir(path = obs.path, pattern = 'dive', full.names = TRUE)
-depths = dir(path = obs.path, pattern = 'depths', full.names = TRUE)
+dives.obs = dives.load(path = cfg$data$path, 
+                       dive_pattern = cfg$data$file_patterns$dive,
+                       depth_pattern = cfg$data$file_patterns$depths)
 
-# storage for observed dives
-dives.obs = vector('list', length(dives))
-
-# load dives 
-for(i in 1:length(dives)) {
-  
-  d = read.csv(file = dives[i], header = TRUE)
-  db = read.csv(file = depths[i], header = TRUE)
-  
-  # center dive times
-  d$times = d$times - d$times[1]
-  
-  dive = as.list(d)
-  class(dive) = 'dsobs'
-  
-  # save
-  dives.obs[[i]] = list(
-    dive = dive,
-    depth.bins = db
-  )
-  
-}
-
-
-# clean up 
-rm(d, db, dive, i, depths, dives, obs.path)
+load(file.path(out.dir, cfg$base_names$fit_inds))
 
 
 #
@@ -73,9 +80,9 @@ tstep = diff(dives.obs[[1]]$dive$times[1:2])
 # load posterior samples locally, and on nodes
 #
 
-load(file.path('fit', 'trace_theta.RData'))
-clusterEvalQ(cl, load(file.path('fit', 'trace_theta.RData')))
-
+load(file.path(out.dir, cfg$base_names$fit))
+clusterExport(cl, c('out.dir', 'cfg'))
+clusterEvalQ(cl, load(file.path(out.dir, cfg$base_names$fit)))
 
 
 #
@@ -83,14 +90,20 @@ clusterEvalQ(cl, load(file.path('fit', 'trace_theta.RData')))
 #
 
 # export additional data to nodes
-clusterExport(cl = cl, c('dives.obs', 'depth.bins', 'tstep'))
+clusterExport(cl = cl, c('dives.obs', 'depth.bins', 'tstep', 'fit.inds'))
 
 # partition posterior samples to distribute across nodes
-dive.nodes = balance_inds(n.inds = nrow(trace), n.partitions = length(cl))
+dive.nodes = balance_inds(n.inds = nrow(state$theta), n.partitions = length(cl))
+
+if(is.null(cfg$sub_paths$imputations)) {
+  defaults = compose_cfg(file = 'conf/config.yaml')
+  cfg$sub_paths$imputations = defaults$sub_paths$imputations
+}
+
 
 # create output directories
-for(dive.id in 1:length(dives.obs)) {
-  dir.create(path = file.path('fit', 'imputations', 
+for(dive.id in fit.inds$fit) {
+  dir.create(path = file.path(out.dir, cfg$sub_paths$imputations, 
                               paste('dive', dive.id, sep='')), recursive = TRUE)
 }
 
@@ -101,8 +114,8 @@ dives.imputed = clusterApply(cl = cl, x = dive.nodes, fun = function(inds) {
   samples = lapply(inds, function(ind) {
     
     # extract posterior sample for model parameters
-    beta = trace[ind, 1:2]
-    lambda = trace[ind,3:5]
+    beta = state$theta[ind, 1:2]
+    lambda = state$theta[ind,3:5]
     
     # compute uniformized transition rate
     rate.unif = max(outer(lambda, 2 * depth.bins[,2], '/'))
@@ -122,33 +135,44 @@ dives.imputed = clusterApply(cl = cl, x = dive.nodes, fun = function(inds) {
     })
     
     # loop over dives
-    x = lapply(1:length(dives.obs), function(dive.id) {
+    x = lapply(1:length(fit.inds$fit), function(dive.ind) {
+      # convert index to dive id
+      dive.id = fit.inds$fit[dive.ind]
+      # extract dive observation
       d = dives.obs[[dive.id]]$dive
-      dsdive.impute(
-        depths = d$depths, times = d$times, 
-        t.stages = trace.t.stages[[ind]][[dive.id]], rate.unif = rate.unif, 
-        P.raw = P.raw, P.tx = P.tx, n.bins = nrow(depth.bins), max.tx = 100)
+      # extract random effects
+      t.stages = state$trace.t.stages[[ind]][[dive.ind]]
+      offset = state$trace.offsets[ind, dive.ind]
+      # align dive observations with offset
+      d.aligned = dsdive.align.obs(depths = d$depths, times = d$times, 
+                                   t.stages = t.stages, offset = offset)
+      # impute dive
+      d.imputed.aligned = dsdive.impute(
+        depths = d.aligned$depths, times = d.aligned$times, t.stages = t.stages, 
+        rate.unif = rate.unif, P.raw = P.raw, P.tx = P.tx, 
+        n.bins = nrow(depth.bins), max.tx = 100)
+      # align imputed dive to observation time-frame
+      d.imputed.aligned$times = d.imputed.aligned$times + offset
+      if(offset > 0) {
+        d.imputed.aligned$times[1] = 0
+        d.imputed.aligned$durations[1] = d.imputed.aligned$durations[1] + offset
+      }
+      # return result
+      d.imputed.aligned
     })
   })
   
   # save outputs by dive id
-  for(dive.id in 1:length(dives.obs)) {
-    imputed = lapply(samples, function(s) { s[[dive.id]] })
-    o = file.path('fit', 'imputations', 
+  for(dive.ind in 1:length(fit.inds$fit)) {
+    dive.id = fit.inds$fit[dive.ind]
+    imputed = lapply(samples, function(s) { s[[dive.ind]] })
+    o = file.path(out.dir, cfg$sub_paths$imputations, 
                   paste('dive', dive.id, sep=''))
     save(imputed, 
          file = file.path(o, paste('dive', dive.id, 'imputed', 
                                    inds[1], inds[length(inds)], 
                                    '.RData', sep = '_')))
   }
-  
-  # # list structure: dive.id : dive.sample
-  # flattened = vector('list', length(dives.obs))
-  # for(dive.id in 1:length(dives.obs)) {
-  #   flattened[[dive.id]] = lapply(samples, function(s) {
-  #     s[[dive.id]]
-  #   })
-  # }
   
   # return 1 for success
   1
