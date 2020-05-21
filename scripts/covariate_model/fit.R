7f28d53b5e2956babf131447d79c1636a12021f9
Josh Hewitt <joshua.hewitt@duke.edu>
2020-05-21 13:21:57 -0600

    test-implement approximate likelihoods

--- scripts/covariate_model/fit.R
+++ scripts/covariate_model/fit.R
@@ -276,7 +276,7 @@ fit = dsdive.gibbs.obs.cov(
   tf.prior.params = unlist(cfg$observation_model$parameters_tf), 
   offsets = offsets, offsets.tf = offsets.tf, covs = dives.obs$covariates, 
   pi.formula = pi.formula, lambda.formula = lambda.formula,
-  warmup = as.numeric(cfg$sampler$warmup), cl = cl)
+  warmup = as.numeric(cfg$sampler$warmup), cl = cl, gapprox = TRUE)
 
 options(error = NULL)
 
