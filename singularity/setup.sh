192cdb1ed7f7a9a546d95e72c7b0f6fbdd2b55e5
Josh Hewitt <joshua.hewitt@duke.edu>
2020-05-21 13:24:07 -0600

    adding new dependency to singularity image

--- singularity/setup.sh
+++ singularity/setup.sh
@@ -43,6 +43,7 @@ singularity exec rmovement.sif install2.r -l libs yaml
 singularity exec rmovement.sif install2.r -l libs expm
 singularity exec rmovement.sif install2.r -l libs bigmemory
 singularity exec rmovement.sif install2.r -l libs Rdsm
+singularity exec rmovement.sif install2.r -l libs fields
 singularity exec rmovement.sif install2.r -l libs -r NULL ./composr
 singularity exec rmovement.sif install2.r -l libs -r NULL ./dsdive
 
